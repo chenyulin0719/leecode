abdf947088c4ed04bec7712f7eb382db2812b38b
kh87313 <kh87313@gmail.com>
2018-09-18 11:19:32 +0800

    Q17 & Q17 test (DFS & BFS way)
    Signed-off-by: kh87313 <kh87313@gmail.com>

--- src/main/java/Solution_17.java
+++ src/main/java/Solution_17.java
@@ -1,11 +1,12 @@
 import java.util.*;
+
 import static java.util.stream.Collectors.toList;
 
 public class Solution_17 {
 
   private Map<Character, List<Character>> numberLetterListMap = new HashMap<>();
 
-  public List<String> letterCombinations(String digits) {
+  Solution_17() {
     numberLetterListMap.put('2', Arrays.asList('a', 'b', 'c'));
     numberLetterListMap.put('3', Arrays.asList('d', 'e', 'f'));
     numberLetterListMap.put('4', Arrays.asList('g', 'h', 'i'));
@@ -14,9 +15,62 @@ public class Solution_17 {
     numberLetterListMap.put('7', Arrays.asList('p', 'q', 'r', 's'));
     numberLetterListMap.put('8', Arrays.asList('t', 'u', 'v'));
     numberLetterListMap.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+  }
 
+  public List<String> letterCombinationsBFS(String digits) {
+    LinkedList<String> res = new LinkedList<>();
+    if (digits == null || digits.length() == 0) {
+      return res;
+    }
+
+    int len = 0;
+    res.add("");
+    for (Character digit : digits.toCharArray()) {
+      len++;
+      List<Character> letterList = numberLetterListMap.get(digit);
+      if (letterList == null) {
+        throw new RuntimeException("Please Enter Number(2-9)");
+      }
+      while (res.peek().length() < len) {
+        String t = res.removeFirst();
+        for (Character c : letterList) {
+          res.add(t + c);
+        }
+      }
+    }
+    return res;
+  }
+
+
+  public List<String> letterCombinationsDFS(String digits) {
+    List<String> res = new ArrayList<>();
+    if (digits == null || digits.length() == 0) {
+      return res;
+    }
+    combinationDFS(new StringBuilder(), res, digits);
+    return res;
+  }
+
+  private void combinationDFS(StringBuilder sb, List<String> res, String digits) {
+    if (digits.isEmpty()) {
+      res.add(sb.toString());
+      return;
+    }
+    List<Character> letterList = numberLetterListMap.get(digits.charAt(0));
+    if (letterList == null) {
+      throw new RuntimeException("Please Enter Number(2-9)");
+    }
+    for (Character c : letterList) {
+      sb.append(c);
+      combinationDFS(sb, res, digits.substring(1));
+      sb.deleteCharAt(sb.length() - 1);
+    }
+  }
+
+  public List<String> letterCombinations(String digits) {
     return combination(new ArrayList<>(), digits);
   }
+
 
   private List<String> combination(List<String> inputList, String digits) {
 
--- src/test/java/Solution_17Test.java
+++ src/test/java/Solution_17Test.java
@@ -6,12 +6,25 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class Solution_17Test {
+  private String input = "23";
+  private List<String> expect = Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf");
+
+  @Test
+  public void letterCombinationsBFS() {
+    Solution_17 instance = new Solution_17();
+    assertEquals(expect, instance.letterCombinationsBFS(input));
+  }
+
+  @Test
+  public void letterCombinationsDFS() {
+    Solution_17 instance = new Solution_17();
+    assertEquals(expect, instance.letterCombinationsDFS(input));
+  }
 
   @Test
   public void letterCombinations() {
     Solution_17 instance = new Solution_17();
-    String input = "23";
-    List<String> expect = Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf");
+
     assertEquals(expect, instance.letterCombinations(input));
   }
 }
