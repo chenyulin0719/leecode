d62fd24bceb0ce2a2ef75112f6097b233cc972bc
Ryan Chen <kh87313@gmail.com>
2022-01-22 01:51:36 +0800

    Fix Q148 typo and update utils.ListNode

--- python/src/main/utils.py
+++ python/src/main/utils.py
@@ -10,3 +10,41 @@ class TreeNode(object):
 
     def add_right(self, val):
         self.right = TreeNode(val)
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __eq__(self, target_node):
+        if self.val != target_node.val:
+            return False
+
+        a = self
+        b = target_node
+
+        while (a is not None) and (b is not None):
+            if a.val != b.val:
+                return False
+
+            a = a.next
+            b = b.next
+
+        return (a is None) and (b is None)
+
+    def __str__(self):
+        a = self
+        temp = []
+        while a is not None:
+            temp.append(a.val)
+            a = a.next
+        return ','.join(map(str, temp))
+
+    def append(self, vals: list):
+        tail = self
+        for i in vals:
+            temp = ListNode(i)
+            tail.next = temp
+            tail = temp
+        return self
